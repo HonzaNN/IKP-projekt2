fe4d7f128eea8a09e22d9985b28a1f6d392fbccc
HonzaNN <jan@novakj.eu>
2023-04-16 16:34:51 +0200

    feat: TCP working

--- ipk-proj2.c
+++ ipk-proj2.c
@@ -10,6 +10,8 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <signal.h>
+#include <unistd.h>
+
 #include <regex.h>
 
 #define BUFSIZE 512
@@ -181,7 +183,7 @@ void UDP_server( char *argv[] ){
     while(1) 
     {   char buf_help[BUFSIZE];
         bzero(buf, BUFSIZE);
-        printf("INFO: Ready.\n");
+        printf("INFO: Ready UDP\n");
     	/* prijeti odpovedi a jeji vypsani */
         clientlen = sizeof(client_address);
         bytesrx = recvfrom(server_socket, buf, BUFSIZE, 0, (struct sockaddr *) &client_address, &clientlen);
@@ -191,10 +193,7 @@ void UDP_server( char *argv[] ){
         hostp = gethostbyaddr((const char *)&client_address.sin_addr.s_addr, 
 			  sizeof(client_address.sin_addr.s_addr), AF_INET);
               
-        //print buffer in hexademical format
-        for(int i = 0; i < buf[1]; i++){
-            printf("%02x, %c.\n", buf[i], buf[i]);
-        }
+        
 
         strcpy(hepl_buf, buf+2);
         buf[0] = 0x01;
@@ -220,11 +219,6 @@ void UDP_server( char *argv[] ){
 
         buf[2] = strlen(buf+3)+3;
         
-        printf("\n \nout data: %s\n", buf+3);
-        for(int i = 0; i < buf[2]; i++){
-            printf("%02x, %c.\n", buf[i], buf[i]);
-        }
-        printf("\n \n");
     
         /* odeslani zpravy zpet klientovi  */        
         bytestx = sendto(server_socket, buf, buf[2], 0, (struct sockaddr *) &client_address, clientlen);
@@ -235,12 +229,189 @@ void UDP_server( char *argv[] ){
 }
 
 
+bool send_message(int socket, char *message){
+    int n = strlen(message);
+    n = write(socket, message, n);
+    if (n < 0) {
+        perror("write failed");
+        return false;
+    }
+    return true;
+}
+
+void handle_client(int new_socket){
+    char buf[BUFSIZE], help_buf[BUFSIZE];
+    int n;
+
+
+    //Check incoming HELLO
+    bzero(buf, BUFSIZETCP);
+    n = read(new_socket, buf, BUFSIZE);
+    if (n < 0) {
+        perror("read failed");
+        return;
+    }
+    if (n == 0) {
+        printf("Client disconnected\n");
+        return;
+    }
+
+
+    if(strcmp(buf, "HELLO\n") == 0){
+        send_message(new_socket, "HELLO\n");
+    }
+    else{
+        send_message(new_socket, "BYE\n");
+
+        close(new_socket);
+        return;
+    }
+
+    result_t result;
+    char *token;
+    //response to client
+    while (1) {
+        // receive data from client
+        bzero(buf, BUFSIZETCP);
+        n = read(new_socket, buf, BUFSIZE);
+        if (n < 0) {
+            perror("read failed");
+            break;
+        }
+        if (n == 0) {
+            perror("Client disconnected\n");
+            break;
+        }
+
+        token = strtok(buf, " ");
+        if(strcmp(token, "BYE") == 0){
+            send_message(new_socket, "BYE\n");
+            break;
+        }
+        else if(strcmp(token, "SOLVE") == 0){
+            token = strtok(NULL, "");
+            if(checkFormat(token)){
+                result = resolve_command(token);
+                if(result.error){
+                    send_message(new_socket, "BYE\n");
+                }
+                else{
+                    sprintf(help_buf, "RESULT %d\n", result.result);
+                    send_message(new_socket, help_buf);
+                }
+            }
+            else{
+                send_message(new_socket, "BYE\n");
+                break;
+            }
+        }
+        else{
+            send_message(new_socket, "BYE\n");
+            break;
+        }
+
+    }
+
+    // close socket
+    close(new_socket);
+
+}
+
+void TCP_server(char *argv[]){
+    char buf[BUFSIZE];
+	int server_socket, port_number, bytestx, bytesrx;
+    socklen_t clientlen;
+    struct sockaddr_in client_address, server_address;
+    int optval;
+    const char * hostaddrp;
+    struct hostent *hostp;
+    pid_t pid;
+
+    int news_socket;
+
+    char *adress;
+    adress = argv[2];
+
+     
+    port_number = atoi(argv[4]);
+    adress = argv[2];
+
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) <= 0)
+	{
+		perror("ERROR: socket");
+		exit(EXIT_FAILURE);
+	}
+
+    /* potlaceni defaultniho chovani rezervace portu ukonceni aplikace */ 
+    optval = 1;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
+
+    /* adresa serveru, potrebuje pro prirazeni pozadovaneho portu */
+    bzero((char *) &server_address, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr(adress);
+    server_address.sin_port = htons((unsigned short)port_number);
+	
+    if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) 
+    {
+        perror("ERROR: binding");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, 5) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    
+    while (1) {
+        //printf("INFO: Ready TCP\n");
+        // accept incoming connection
+        clientlen = sizeof(client_address);
+        news_socket = accept(server_socket, (struct sockaddr *)&client_address, &clientlen);
+        if (news_socket < 0) {
+            //perror("accept failed");
+            continue;
+        }
+
+
+        // fork new process to handle client
+        pid = fork();
+        if (pid < 0) {
+            perror("fork failed");
+            close(news_socket);
+            continue;
+        }
+        if (pid == 0) {  // child process
+            close(server_socket);
+            handle_client(news_socket);
+            exit(EXIT_SUCCESS);
+        }
+        else {  // parent process
+            close(news_socket);
+        }
+    }
+
+
+}
+
+
 int main(int argc, char *argv[]){
     if(!checkArgs(argc, argv)){
         return 1;
     }
     //test();
-    UDP_server(argv);
+
+    if(strcmp(argv[6], "tcp") == 0){
+        TCP_server(argv);
+    }
+    else if(strcmp(argv[6], "udp") == 0){
+        UDP_server(argv);
+    }
+    else{
+        perror("ERROR: Unknown protocol\n");
+        return 1;
+    }
     return 0;
 
     
